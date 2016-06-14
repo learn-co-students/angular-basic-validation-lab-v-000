49368f0983b25f965a3c190514ddb5723aff228b
Louiefigz <figueroa.aaron@gmail.com>
2016-06-14 17:01:33 -0400

    Done.

--- js/app/controllers/FormController.js
+++ js/app/controllers/FormController.js
@@ -1,5 +1,29 @@
 function FormController($scope) {
-
+  // $scope.form = {
+  //   username: {
+  //       $untouched: true,
+  //       $touched: false,
+  //       $pristine: true,
+  //       $dirty: false,
+  //       $valid: true,
+  //       $invalid: false,
+  //       $minlength: 3,
+  //       $error: {
+  //         required: true
+  //       }
+  //   },
+  //   password: {
+  //       $untouched: true,
+  //       $touched: false,
+  //       $pristine: true,
+  //       $dirty: false,
+  //       $valid: true,
+  //       $invalid: false,
+  //       $error: {
+  //         required: true
+  //       }
+  //   }
+  // };
 }
 
 angular
