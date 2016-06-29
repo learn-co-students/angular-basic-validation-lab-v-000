48a262505a443890a21db2f49b9a052913f8562e
the-widget <brennenawana108ny@gmail.com>
2016-06-29 14:51:41 -0400

    Done.

--- js/app/controllers/FormController.js
+++ js/app/controllers/FormController.js
@@ -1,7 +1,20 @@
 function FormController($scope) {
-
+  
 }
+
 
 angular
     .module('app')
     .controller('FormController', FormController);
+
+
+// $scope.form = {
+//     inputName: {
+//         $untouched: true,
+//         $touched: false,
+//         $pristine: true,
+//         $dirty: false,
+//         $valid: true,
+//         $invalid: false,
+//         $error: {}
+//     }
