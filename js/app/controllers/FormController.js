a06591392467bce8f831ebd5854c71d7c2a7de9d
Tyler Smith <oats000@hotmail.com>
2016-06-29 01:42:52 +0000

    Done.

--- js/app/controllers/FormController.js
+++ js/app/controllers/FormController.js
@@ -1,5 +1,40 @@
 function FormController($scope) {
+  $scope.form = {
+    username: {
+      $untouched: true,
+      $touched: false,
+      $pristine: true,
+      $dirty: false,
+      $valid: false,
+      $invalid: true,
+      $error: {
+          required: true
+      }
+    },
+    password: {
+      $untouched: true,
+      $touched: false,
+      $pristine: true,
+      $dirty: false,
+      $valid: false,
+      $invalid: true,
+      $error: {
+          required: true
+      }
+    },
+    email: {
+      $untouched: true,
+      $touched: false,
+      $pristine: true,
+      $dirty: false,
+      $valid: false,
+      $invalid: true,
+      $error: {
+          required: true
+      }
+    },
 
+  };
 }
 
 angular
