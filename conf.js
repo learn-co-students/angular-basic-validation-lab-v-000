26f5ea202e4a62cfbeae2935d3f643951873cb8b
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-07-11 18:59:58 -0700

    passes 9 of 10 tests

--- conf.js
+++ conf.js
@@ -1,11 +1,5 @@
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: ['spec/server.js', 'spec/**/*.spec.js'],
-	rootElement: '.app',
-	capabilities: {
-	  browserName: 'chrome',
-	  'chromeOptions': {
-	    args: ['--no-sandbox'] 
-	  }   
-	}
+	rootElement: '.app'
 };
