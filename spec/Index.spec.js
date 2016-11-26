03b57655d229917117ff89679077fe77d78b6f78
Joseph Lim <joseph.d.lim@gmail.com>
2016-11-26 07:44:13 -0500

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -20,7 +20,7 @@ describe('Events Test', function() {
 			username.sendKeys('aa');
 			expect(messages.getText()).toContain('3');
 		});
-		
+
 		it('should make sure the username maxlength is 20 characters', function() {
 			username.sendKeys(protractor.Key.BACK_SPACE, protractor.Key.BACK_SPACE);
 			username.sendKeys('abcdefghijklmnopqrstuvwxyz');
@@ -28,6 +28,7 @@ describe('Events Test', function() {
 		});
 
 		it('should not say anything when valid', function() {
+			username.sendKeys(protractor.Key.BACK_SPACE, protractor.Key.BACK_SPACE, protractor.Key.BACK_SPACE, protractor.Key.BACK_SPACE, protractor.Key.BACK_SPACE, protractor.Key.BACK_SPACE);
 			expect(messages.getText()).toEqual('');
 		});
 	});
